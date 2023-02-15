166e16647ab3c927709564b5a4d370f27cf8a342
Mighty Eagle <mightyeaglecorp@gmail.com>
2023-02-15 15:49:26 +0100

    made changes

---
+++ src/httpService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const httpService = axios.create({
+  baseURL: `${window.location.origin.replace(":3000", ":4000")}/api/v1/`,
+  timeout: 10000,
+  //withCredentials: "include",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+httpService.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    return { error: error.response.data };
+  }
+);
+
+export { httpService };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,36 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Typography } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import { Login } from "@mui/icons-material";
+import { httpService } from "../httpService";
+import SnackBar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import { forwardRef } from "react";
 
 export default function HomePage() {
+  const [regNumber, setRegNumber] = useState("");
+  const [message, setMessage] = useState("");
+  const [severity, setSeverity] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const Alert = forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+  const submitForm = async (e) => {
+    e.preventDefault();
+
+    const { data, error } = await httpService.post("login", { regNumber });
+
+    if (error) {
+      setOpen(true);
+      setSeverity("error");
+      setMessage(error);
+    }
+    console.log(error);
+  };
   return (
     <div className="row m-0">
       <div className="col-lg-6 homeImg"></div>
       <div className="col-lg-6 d-flex align-items-center">
-        <div className="col-lg-6">
-          <div className="mb-3">
-            <Typography variant="h3" fontWeight={900} color="#2a3eb1">
+        <div className="col-lg-6 ms-4">
+          <div className="mb-4">
+            <Typography
+              variant="h3"
+              fontWeight={900}
+              color="#2a3eb1"
+              textAlign={"center"}
+            >
               AGUILA
             </Typography>
           </div>
           <div>
-            <TextField
-              fullWidth
-              label="Registration Number"
-              helperText="Candidate registration number"
-            />
-            <LoadingButton
-              variant="outlined"
-              fullWidth
-              endIcon={<Login />}
-              className="mt-3"
-            >
-              <span>login</span>
-            </LoadingButton>
+            <form onSubmit={submitForm}>
+              <TextField
+                onChange={(e) => setRegNumber(e.target.value)}
+                fullWidth
+                label="Registration Number"
+                helperText="Candidate registration number"
+              />
+              <LoadingButton
+                type="submit"
+                variant="outlined"
+                fullWidth
+                endIcon={<Login />}
+                className="mt-3"
+              >
+                <span>login</span>
+              </LoadingButton>
+            </form>
           </div>
         </div>
       </div>
+
+      <SnackBar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={open}
+        autoHideDuration={5000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity={severity} sx={{ width: "25%" }}>
+          {message}
+        </Alert>
+      </SnackBar>
     </div>
   );
 }
